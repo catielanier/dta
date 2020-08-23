a5154633b3df34bc4304faded9ca4f4115c204ae
Corey Lanier <iam@coreylanier.com>
2020-08-23 16:46:39 -0400

    added icons to render

--- src/components/Sites.vue
+++ src/components/Sites.vue
@@ -1,13 +1,24 @@
 <template>
-  <div class="sitelist">
-    <div class="sitename">Site</div>
-    <div class="date">Date Created</div>
-    <div class="actions">Actions</div>
+  <div class="sitelist rounded-sm">
+    <div class="sitename font-semibold bg-white text-gray-800 border border-white p-1">Site</div>
+    <div class="date font-semibold bg-white text-gray-800 border border-white p-1">Date Created</div>
+    <div class="actions font-semibold bg-white text-gray-800 border border-white p-1">Actions</div>
     <Fragment v-for="(site, index) in passwords" :key="index">
-      <div class="sitename">{{site.siteName}}</div>
-      <div class="date">{{site.dateCreated}}</div>
-      <div class="actions">
-        <button @click.prevent="copyToClipboard(site.siteName)">Copy</button>
+      <div class="sitename border border-white p-1">{{site.siteName}}</div>
+      <div class="date border border-white p-1">{{formatDate(site.dateCreated)}}</div>
+      <div class="actions border border-white p-1">
+        <button class="text-orange-700 px-1" @click.prevent="copyToClipboard(site.siteName)">
+          <font-awesome-icon :icon="['fas', 'copy']" />
+        </button>
+        <button class="text-orange-700 px-1">
+          <font-awesome-icon :icon="['fas', 'edit']" />
+        </button>
+        <button class="text-orange-700 px-1">
+          <font-awesome-icon :icon="['fas', 'sync-alt']" />
+        </button>
+        <button class="text-orange-700 px-1" @click.prevent="removePassword(site.siteName)">
+          <font-awesome-icon :icon="['fas', 'trash']" />
+        </button>
       </div>
     </Fragment>
   </div>
@@ -23,12 +34,25 @@ export default {
   },
   components: { Fragment },
   methods: {
-    ...mapActions(["copyPassword"]),
+    ...mapActions(["copyPassword", "deletePassword"]),
     copyToClipboard: function(siteName) {
-      console.log(siteName);
-      console.log("copying password");
       this.copyPassword({ siteName });
+    },
+    removePassword: function(siteName) {
+      this.deletePassword({ siteName });
+    },
+    formatDate: function(date) {
+      const newDate = new Date(date);
+      return newDate.toLocaleString("en-us", { dateStyle: "long" });
     }
   }
 };
 </script>
+
+<style>
+.sitelist {
+  text-align: left;
+  display: grid;
+  grid-template-columns: 1fr 0.3fr 0.2fr;
+}
+</style>
--- src/main.js
+++ src/main.js
@@ -1,14 +1,26 @@
 import Vue from "vue";
+import { library } from "@fortawesome/fontawesome-svg-core";
+import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
+import {
+	faCopy,
+	faEdit,
+	faTrash,
+	faSyncAlt
+} from "@fortawesome/free-solid-svg-icons";
 import App from "./App.vue";
 import router from "./router";
 import store from "./store";
+
+library.add(faCopy, faEdit, faTrash, faSyncAlt);
+
+Vue.component("font-awesome-icon", FontAwesomeIcon);
 
 import "./assets/styles/index.css";
 
 Vue.config.productionTip = false;
 
 new Vue({
-  router,
-  store,
-  render: (h) => h(App),
+	router,
+	store,
+	render: h => h(App)
 }).$mount("#app");
